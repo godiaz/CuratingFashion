00e3a2d8fc50bec54dbccad647865bd6122e629a
artmai <arthurmaisonnier@gmail.com>
2018-06-19 19:15:14 +0100

    removed DynamicLoader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,8 @@
 import "bootstrap";
 import "flatpickr/dist/flatpickr.min.css";
 import "flatpickr/dist/themes/light.css";
-import { loadDynamicBannerText } from '../components/banner';
 
 
 import { autocomplete } from '../components/autocomplete';
 
 autocomplete();
-loadDynamicBannerText();
