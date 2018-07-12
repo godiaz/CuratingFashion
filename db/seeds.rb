445fb07b7de1d9e9e8c2df7f8f476d7b7bfa3053
Gonzalo Diaz <info@ventra.com.co>
2018-07-12 10:17:24 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([{ first_name: "Gonzalo" last_name: "Diaz" email: "gonzalodiaz000@gmail.com", admin: true, address: "19 Pitfield Street, London, United Kingdom"}])
+users = User.create([{ first_name: "Gonzalo" last_name: "Diaz" email: "gonzalodiaz000@gmail.com", password: 'gonzalo1234', password_confirmation: 'gonzalo1234', address: "19 Pitfield Street, London, United Kingdom"}])
+
+admin=User.new({ email: 'gonzalodiaz000@gmail.com',
+           password: 'gonzalo1234', password_confirmation: 'gonzalo1234'})
+
+admin.toggle!(:admin)
+
+if admin.valid?
+  admin.save()
+
+elsif admin.errors.any?
+  admin.errors.full_messages.each do |msg|
+    puts msg
+  end
+else
+  puts "****NOT VALID****"
+end
